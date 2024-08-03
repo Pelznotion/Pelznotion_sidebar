d013ca75292a22d75be4be4fa590b0c043aeaee5
Pelznotion <145939369+Pelznotion@users.noreply.github.com>
2024-08-03 10:36:22 -1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const expandBtn = document.querySelector(".expand-btn");
+const img = expandBtn.querySelector("img");
+let activeIndex;
+expandBtn.addEventListener("click", () => {
+  document.body.classList.toggle("collapsed");
+});
+const current = window.location.href;
+const allLinks = document.querySelectorAll(".sidebar-links a");
+allLinks.forEach((elem) => {
+  elem.addEventListener("click", function () {
+    const hrefLinkClick = elem.href;
+    allLinks.forEach((link) => {
+      if (link.href == hrefLinkClick) {
+        link.classList.add("active");
+      } else {
+        link.classList.remove("active");
+      }
+    });
+  });
+});
